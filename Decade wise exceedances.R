30d0d19b7b74f4e1bd030a7613037a1cc54a2dc9
Sharvil16 <136013589+Sharvil16@users.noreply.github.com>
2024-08-13 14:07:14 +0530

    Update Decade wise exceedances.R

--- Decade wise exceedances.R
+++ Decade wise exceedances.R
@@ -1,20 +1,24 @@
 library(pacman)
 pacman::p_load(pacman, rio)
-colaba <- import("C:\\Users\\admin\\Desktop\\colaba.csv")
-pune <- import("C:\\Users\\admin\\Documents\\GitHub\\ISPS2024\\daily_rainfall_pune.csv")
+
+lat_long_cities <- import("C:\\Users\\admin\\Documents\\GitHub\\ISPS2024\\city_lat_long.xlsx")
+names <- c(lat_long_cities$City)
+
+
+
+
 date_series1 <- seq(as.Date("1951-01-01"), as.Date("2023-12-31"), by = "day")
 date_series2 <- seq(as.Date("1954-01-01"), as.Date("2023-12-31"), by = "day")
-
-city_data <- list(colaba = colaba, pune = pune)
 
 decades = c("1954-1963", "1964-1973", "1974-1983", 
             "1984-1993", "1994-2004", "2004-2013", 
             "2014-2023")
 exceedance_df <- data.frame(Decade = decades)
 
-for (city_name in names(city_data)) {
-  print(city_name)
-  city <- city_data[[city_name]]
+
+for (name in names){
+  city <- read.csv(paste0("C:\\Users\\admin\\Documents\\GitHub\\ISPS2024\\city\\",name,".csv"), header=F, sep=",")
+  
   
   values1 <-unlist(city, use.names=F)
   values2 <- values1[1097:length(values1)]
@@ -25,7 +29,7 @@ for (city_name in names(city_data)) {
                            breaks = seq(1954, 2024, by = 10), 
                            labels = c("1954-1963", "1964-1973", "1974-1983", 
                                       "1984-1993", "1994-2004", "2004-2013", 
-                                       "2014-2023"), 
+                                      "2014-2023"), 
                            right = FALSE)
   
   
@@ -35,9 +39,10 @@ for (city_name in names(city_data)) {
   
   #exceedance_counts <- as.integer(sapply(decades, function(d) exceedance_counts[d]))
   
-  exceedance_df[[city_name]] <- exceedance_counts
+  exceedance_df[[name]] <- exceedance_counts
   
-}
+  
+  }
 
 exceedance_df
 
